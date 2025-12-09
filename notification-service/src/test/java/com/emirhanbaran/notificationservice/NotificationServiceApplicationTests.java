42cbb7cf0c03b6b7cd4849619e32040a009df532
Emirhan Baran <emir.baran255@gmail.com>
2025-12-09 21:47:40 +0300

    initialize project structure with multiple services and configuration files

---
+++ notification-service/src/test/java/com/emirhanbaran/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emirhanbaran.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
